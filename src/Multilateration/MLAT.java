dca6828acf4e7365ddb14771f8d6c51cbf65c9e3
JamesBadham <jbadham95@gmail.com>
2018-02-21 21:03:53 +0000

    Added sections requiring usage of LogData class.

--- src/Multilateration/MLAT.java
+++ src/Multilateration/MLAT.java
@@ -1,19 +1,25 @@
 package Multilateration;
 
 import Jama.Matrix;
+import java.util.ArrayList;
 
 public class MLAT {
     static int no_of_radios;
     static int radioIndex=0; //get from user maybe? 
-                            //find a way to pass the radio I am computing for
-    //maybe I can get this from Primer, since it's the class that has the data
+    
+
+    
+    
     
     public static void main(String args[]){    
+        //ArrayList to store the Data extracted from all Log files.
+        ArrayList<LogData> dataArr = new ArrayList<LogData>();
         // sketch out the flow of the program:
         // 0. create a program instance:
         PrimerClass primer= new PrimerClass();
         // 1. set no. of radios in field
         primer.setNumberOfRadios(5); // change to user input
+        //
         // 2. insert all radio coordinates
         for (int i=0; i<no_of_radios; i++){
             //user input
@@ -24,18 +30,26 @@ public class MLAT {
             //unser input
             //repeated call to primer.setRadioMeasuredPower()
         }
+        // 3.5. Extract data from all relevant log files.
+        for (int i=0;i<no_of_radios;i++){
+            //Ask user to input the full path of a log file.
+            String fPath = "/Path/Name";
+            //Create a new instance of LogData with path.
+            LogData log = new LogData(fPath);
+            //Add LogData object to ArrayList.
+            dataArr.add(log);
+        }
         // 4. get all rssi values
-        //parse the file - i know which radio it is by the file
-        //for each "tracking detection" line
-        //get the tag number
-        //get the time of the detection
-        //get the rssi
-        //if the line is a bust, so no detection or it says -infinity, 
-        //put rssi down as 0
         for (int i=0; i<no_of_radios; i++){
-            //get parsed values
-            //ordered list of tagID and corresponding list of time and rssi
-            //repeated call to primer.setTimeRssiValues()
+            //Retrieve LogData object.
+            LogData log = dataArr.get(i);
+            //Get times for this base station.
+            ArrayList<Long> tData = log.getTimes();
+            //Get IDs for this basestation.
+            ArrayList<Long> idData = log.getIDs();
+            //Get RSSI values for this basestation.
+            ArrayList<Double> rssiData = log.getRSSIs();
+            primer.setTimeRssiValues(tData,idData,rssiData);
         }
         // 5. find out all distances using RSSI
         for (int i=0; i<no_of_radios; i++){
@@ -60,5 +74,6 @@ public class MLAT {
         Matrix sol= eq.getSolution(); //these are our coordinates
                                       //the matrix is 4x1, and entries 1,2,3
                                       //give us the x, y, z coord
+
     }
 }
