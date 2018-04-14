293fb1f45c565e7b4f8ba214b9577de3f24afc1b
ed343 <ed343@users.noreply.github.com>
2018-04-14 12:09:00 +0100

    fixed prepop bug

--- src/GUI/BasestationDB.java
+++ src/GUI/BasestationDB.java
@@ -8,11 +8,11 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class BasestationDB {
-    
+
     // the URL where we can create/find the database
     // will always be in the current directory - ".\\"
-    static String url="jdbc:sqlite:.\\";
-    
+    static String url = "jdbc:sqlite:.\\";
+
     /**
      * Connect to database and create a basestation table if one doesn't exist.
      *
@@ -20,43 +20,49 @@ public class BasestationDB {
      */
     public static void createNewDatabase(String fileName) {
         // append the name of the db to the URL
-        url += fileName;
-        // connect to the database
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                DatabaseMetaData meta = conn.getMetaData();
-                Statement stmt = conn.createStatement();
-                // create a table for basestations if one doesn't exist
-                String sql = "CREATE TABLE IF NOT EXISTS BASESTATIONS " +
-                             "(name VARCHAR(20) not NULL, " +
-                             " LATITUDE DOUBLE PRECISION not NULL, " +
-                             " LONGITUDE DOUBLE PRECISION not NULL, " +
-                             " MEASUREDPOWER DOUBLE PRECISION not NULL, " +
-                             " PRIMARY KEY ( name ))"; 
-                stmt.executeUpdate(sql);
+        if (url.contains(fileName)) {
+            // do nothing, DB is already there
+        } else {
+            //create DB, one doesn't exist yet
+            url += fileName;
+            // connect to the database
+            try (Connection conn = DriverManager.getConnection(url)) {
+                if (conn != null) {
+                    DatabaseMetaData meta = conn.getMetaData();
+                    Statement stmt = conn.createStatement();
+                    // create a table for basestations if one doesn't exist
+                    String sql = "CREATE TABLE IF NOT EXISTS BASESTATIONS "
+                            + "(name VARCHAR(20) not NULL, "
+                            + " LATITUDE DOUBLE PRECISION not NULL, "
+                            + " LONGITUDE DOUBLE PRECISION not NULL, "
+                            + " MEASUREDPOWER DOUBLE PRECISION not NULL, "
+                            + " PRIMARY KEY ( name ))";
+                    stmt.executeUpdate(sql);
+                }
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
         }
     }
+
     /**
-     * The method inserts a row into the basestation table. It reuses the 
-     * connection created in BasestationController, so that the db does not 
+     * The method inserts a row into the basestation table. It reuses the
+     * connection created in BasestationController, so that the db does not
      * become locked.
-     * 
+     *
      * @param name the name of the basestation
      * @param latitude the latitude of the basestation
      * @param longitude the longitude of the basestation
      * @param measuredPower the measured power of the basestation
      * @param c connection to the database
      */
-    public static void insert(String name, Double latitude, Double longitude, 
-                                           Double measuredPower, Connection c) {
-        String sql = "INSERT INTO basestations(name, latitude, longitude, " + 
-                     "measuredpower) VALUES(?,?,?,?)";
+    public static void insert(String name, Double latitude, Double longitude,
+            Double measuredPower, Connection c) {
+        String sql = "INSERT INTO basestations(name, latitude, longitude, "
+                + "measuredpower) VALUES(?,?,?,?)";
         try {
             // insert the appropiate values into the sql statement
-            PreparedStatement pstmt = c.prepareStatement(sql); 
+            PreparedStatement pstmt = c.prepareStatement(sql);
             pstmt.setString(1, name);
             pstmt.setDouble(2, latitude);
             pstmt.setDouble(3, longitude);
@@ -67,21 +73,22 @@ public class BasestationDB {
             System.out.println(e.getMessage());
         }
     }
+
     /**
-     * The method updates a row into the basestation table. It reuses the 
-     * connection created in BasestationController, so that the db does not 
+     * The method updates a row into the basestation table. It reuses the
+     * connection created in BasestationController, so that the db does not
      * become locked.
-     * 
+     *
      * @param name the name of the basestation
      * @param latitude the latitude of the basestation
      * @param longitude the longitude of the basestation
      * @param measuredPower the measured power of the basestation
      * @param c connection to the database
      */
-    public static void update(String name, Double latitude, Double longitude, 
-                                           Double measuredPower, Connection c) {
-        String sql = "UPDATE basestations SET latitude=?, longitude=?," + 
-                     "measuredpower=? WHERE name=?";
+    public static void update(String name, Double latitude, Double longitude,
+            Double measuredPower, Connection c) {
+        String sql = "UPDATE basestations SET latitude=?, longitude=?,"
+                + "measuredpower=? WHERE name=?";
         try {
             // insert the appropiate values into the sql statement
             PreparedStatement pstmt = c.prepareStatement(sql);
@@ -96,10 +103,11 @@ public class BasestationDB {
             System.out.println(e.getMessage());
         }
     }
-    
+
     // FUTURE -- DELETE
     /**
      * Method deletes a basestation entry from the database.
+     *
      * @param name the name of the basestation to be deleted
      * @param c connection to the database
      */
