04403fb51408b93cf91a96c55c5784d76f906a22
JamesBadham <jbadham95@gmail.com>
2018-04-23 18:49:54 +0100

    Create TestSuite.java

---
+++ test/TestSuite.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Multilateration;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+   LogDataTest.class,
+   PrimerClassTest.class,
+   RssiEquationTest.class,
+   MLATEquationTest.class
+        
+})
+
+public class TestSuite {   
+}  
